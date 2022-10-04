5d61841a240e7a24484f1fd8b1ba0efc50e668f9
rickwest <rickwest@users.noreply.github.com>
2022-10-04 09:50:30 +0000

    Fix styling

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -4,7 +4,6 @@ use Illuminate\Contracts\Foundation\Application;
 use RickWest\SportsPress\SportsPress;
 
 if (! function_exists('sportspress')) {
-
     /**
      * @return Application|mixed|SportsPress
      *
